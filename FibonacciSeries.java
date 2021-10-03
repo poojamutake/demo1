ef8102ecc603c9a81d1e05a805f50e3a46c86320
poojamutake <91819369+poojamutake@users.noreply.github.com>
2021-10-03 13:14:17 +0530

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,19 @@
+package day4;
+
+public class FibonacciSeries {
+ public static void main(String args[]) {
+		      int a, b, c, i, n;
+		      n = 10;
+		      a = b = 1;
+		      System.out.print(a+" "+b);
+		      for(i = 1; i <= n-2; i++) {
+		         c = a + b;
+		         System.out.print(" ");
+		         System.out.print(c);
+		         a = b;
+		         b = c;
+		      }
+		   }
+		}
+
+//o/p 1 1 2 3 5 8 13 21 34 55
